a73e64e2add70339a0bd6fbfcafffaad7b0ddb15
Carson Rajcan <crajcan@enova.com>
2019-03-03 22:32:29 -0600

    Practice using enum implicit and explicit descriminators

--- custom_types/use.rs
+++ custom_types/use.rs
@@ -4,8 +4,21 @@ enum Status {
 }
 
 enum Work {
-     Civilian, 
+     Civilian,
      Soldier,
+}
+
+enum Number{
+     foo,
+     One,
+     Two,
+     bar,
+}
+
+enum Color {
+    Red   = 0xff0000,
+    Green = 0x00ff00,
+    Blue  = 0x0000ff,
 }
 
 fn main() {
@@ -24,4 +37,11 @@ fn main() {
         Civilian => println!("Civilians work!"),
         Soldier  => println!("Soldiers fight!"),
     }
+
+    println!("foo is {}",  Number::foo as i32);
+    println!("one is {}",  Number::One as i32);
+    println!("bar is {}",  Number::bar as i32);
+
+    println!("roses are #{:06x}", Color::Red as i32);
+    println!("violets are #{:06x}", Color::Blue as i32);
 }
