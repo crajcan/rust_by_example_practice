359171c7772528fdd5f6ee19b3f3462a2bf80cce
Carson Rajcan <carajcan@gmail.com>
2019-03-06 19:33:17 -0600

    Mess with destructuring structs

--- control_flow/destructuring.rs
+++ control_flow/destructuring.rs
@@ -11,5 +11,23 @@ fn main() {
         (x, y) => println!("`x` is `{:?}` and `y` is `{:?}`", x, y),
         // `_` means don't bind the value to a variable
     }
+
+    struct Foo { x: (u32, u32), y: u32 }
+    let foo = Foo { x: (1,2), y: 3 };
+    let Foo { x: (a, b), y } = foo;
+
+    println!("a = {}, b = {}, y = {}", a, b, y);
+
+    //ordering doesn't matter for destructuring with named fields,
+    //this will maintain x and y from foo
+    let Foo { y: i, x: j }  = foo;
+    println!("i = {:?}, j = {:?}", i, j);
+
+    let Foo { y, .. } = foo;
+    println!("y = {}", y);
+
+    // This will give an error: poatter does not mention field `x`
+    //just like with matching!
+    //let Foo { y } = foo;
 }
 
