e6bc56483c155778c95582aefdeece8e2fd1e5f5
Carson Rajcan <carajcan@gmail.com>
2019-03-17 22:32:36 -0500

    Playing with closures and borrowing a little more

---
+++ functions/move.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let haystack = vec![1, 2, 3];
+  
+    let contains = move |needle| haystack.contains(needle);
+
+    println!("{}", contains(&1));
+
+    //println!("There're {} elements in vec", haystack.len()); 
+}
