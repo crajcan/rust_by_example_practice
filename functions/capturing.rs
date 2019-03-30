8efc76a8ff66c63cc9e0b95abedc7842e2d346b7
Carson Rajcan <carajcan@gmail.com>
2019-03-30 06:56:43 -0500

    put move example next to capturing example

--- functions/capturing.rs
+++ functions/capturing.rs
@@ -28,5 +28,12 @@ fn main() {
     };
 
     consume();
-    //consume();
+
+    let haystack = vec![1, 2, 3];
+
+    println!("There're {} elements in vec", haystack.len());
+    let contains = move |needle| haystack.contains(needle);
+
+    println!("{}", contains(&1));
+    println!("{}", contains(&4));
 }
